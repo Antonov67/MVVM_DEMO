3069ae6a121db1cc8a8472cd886ba163723a2440
Dm Ant <Dm@Ant.ru>
2023-01-30 10:56:46 +0300

    вывод результатов запроса в textView

--- app/src/main/java/com/example/mvvm_demo/ui/MainActivity.java
+++ app/src/main/java/com/example/mvvm_demo/ui/MainActivity.java
@@ -8,8 +8,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.mvvm_demo.dao.OrdersEntity;
+import com.example.mvvm_demo.dao.OrdersWithUsersEntity;
 import com.example.mvvm_demo.dao.UserEntity;
 import com.example.mvvm_demo.response.PoetryDBService;
 import com.example.mvvm_demo.response.RandomPoem;
@@ -26,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "mvvm";
 
     private MainViewModel mainViewModel;
+    private TextView resulText;
 
     private Button dbButton, responseButton;
 
@@ -38,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
         dbButton = findViewById(R.id.button);
         responseButton = findViewById(R.id.response_button);
+        resulText = findViewById(R.id.result_text);
 
         dbButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,12 +57,17 @@ public class MainActivity extends AppCompatActivity {
                 });
 
                 Log.d(TAG, "orders");
-                mainViewModel.getAllOrders().observe(MainActivity.this, new Observer<List<OrdersEntity>>() {
+                mainViewModel.getAllOrdersWithUsers().observe(MainActivity.this, new Observer<List<OrdersWithUsersEntity>>() {
                     @Override
-                    public void onChanged(List<OrdersEntity> list) {
-                        for (OrdersEntity orders: list) {
-                            Log.d(TAG, orders.orderInfo + " " + orders.userId + " " + orders.date);
+                    public void onChanged(List<OrdersWithUsersEntity> list) {
+                        StringBuilder stringBuilder = new StringBuilder();
+                        for (OrdersWithUsersEntity orders: list) {
+                            Log.d(TAG, orders.usersEntity.name + " " + orders.ordersEntity.orderInfo + " " + orders.ordersEntity.userId + " " + orders.ordersEntity.date);
+                            stringBuilder
+                                    .append(orders.usersEntity.name + ": " + orders.ordersEntity.orderInfo + ", " + orders.ordersEntity.userId + ", " + orders.ordersEntity.date)
+                                    .append("\n\n");
                         }
+                        resulText.setText(stringBuilder);
                     }
                 });
             }
@@ -70,8 +79,22 @@ public class MainActivity extends AppCompatActivity {
                 mainViewModel.getPoems().observe(MainActivity.this, new Observer<List<RandomPoem>>() {
                     @Override
                     public void onChanged(List<RandomPoem> list) {
+                        StringBuilder stringBuilder = new StringBuilder();
                         for (RandomPoem randomPoem : list) {
+                            stringBuilder
+                                    .append("автор:").append("\n\n")
+                                    .append(randomPoem.author)
+                                    .append("\n\n")
+                                    .append("название:").append("\n\n")
+                                    .append(randomPoem.title)
+                                    .append("\n\n")
+                                    .append("количество строк: ")
+                                    .append(randomPoem.linecount)
+                                    .append("\n\n")
+                                    .append("поэма:").append("\n\n")
+                                    .append(randomPoem.getPoemText());
                             Log.d(TAG,randomPoem.author + " " +  randomPoem.title + " " + randomPoem.getPoemText());
+                            resulText.setText(stringBuilder.toString());
                         }
                     }
                 });
